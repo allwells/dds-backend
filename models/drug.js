7ed36828df3528e811d841854aec16832778a727
allwells <aleenfestus@gmail.com>
2021-01-18 02:39:27 +0100

    Prisma initialzation

--- models/drug.js
+++ models/drug.js
@@ -1,22 +1,4 @@
-const mongoose = require("mongoose");
+const config = require("../utils/config");
+const { PrismaClient } = require("@prisma/cli");
 
-const drugSchema = mongoose.Schema({
-  serial: String,
-  drug_name: String,
-  manufacture_date: Date,
-  expiry_date: Date,
-  nafdac_no: String,
-  net_weight: Number,
-  type: String,
-  producer: String,
-});
-
-drugSchema.set("toJSON", {
-  transform: (document, returnedObject) => {
-    returnedObject.id = returnedObject._id.toString();
-    delete returnedObject._id;
-    delete returnedObject.__v;
-  },
-});
-
-module.exports = mongoose.model("Drug", drugSchema);
+const prisma = new PrismaClient();
